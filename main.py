92b549b0742e6f67c73a66ca716165fa663592d1
radie <Radievskiy2003@gmail.com>
2023-09-18 23:16:31 +0300

    Исправил ошибки

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import yfinance as yf
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
 import numpy as np
 
 def fetch_data():
@@ -8,45 +8,42 @@ def fetch_data():
     data = yf.download(ticker, start="2021-01-01", end="2023-01-01", interval="1mo")
     return data
 
-# def parse_data():
-#     url = "https://finance.yahoo.com/quote/TON11419-USD/history?period1=1630022400&period2=1694908800&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true"
-#     response = requests.get(url)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-#
-#     table = soup.find('table', {'data-test': 'historical-prices'})
-#     rows = table.find_all('tr')
-#
-#     data = []
-#     for row in rows[1:]:
-#         columns = row.find_all('td')
-#         date = columns[0].text
-#         close_price = columns[4].text
-#         data.append((date, close_price))
-#
-#     df = pd.DataFrame(data, columns=['Date', 'Close Price'])
-#     return df
-
+# 1. Результати в файлик
 def save_to_csv(df):
     df.to_csv('parsed_data.csv')
 
+# 2. Оцінка тренду реальних данних
 def plot_trend(df):
-    df['Close'].plot()
-    plt.title('Dynamics of TON Coin')
+    df['Close'].plot(figsize=(10, 6), title='Dynamics of TON Coin', grid=True, color='blue', linewidth=2)
     plt.show()
 
+# 3. Статистична характеристика
 def get_statistics(df):
     return df.describe()
 
-def model_data(df):
-    X = np.array(range(len(df))).reshape(-1, 1)
-    y = df['Close'].values
+# 4. Нормальні та аномальні
+def calculate_errors(df):
+    # Середнє та відхилення
+    mean = df['Close'].mean()
+    std = df['Close'].std()
 
-    model = LinearRegression()
-    model.fit(X, y)
-    df['Predicted'] = model.predict(X)
+    # Нормальна
+    df['Normal_Error'] = std / np.sqrt(len(df))
 
-    df[['Close', 'Predicted']].plot()
-    plt.title('Real vs Predicted Data')
+    # Аномалка
+    df['Anomaly_Error'] = df['Close'] - mean
+
+    return df
+
+# 5. Візуал
+def plot_errors(df):
+    plt.figure(figsize=(10, 6))
+    plt.plot(df.index, df['Close'], label='Actual Data', color='blue', linewidth=2)
+    plt.fill_between(df.index, df['Close'] - df['Normal_Error'], df['Close'] + df['Normal_Error'], color='gray', alpha=0.5, label='Normal Error')
+    plt.fill_between(df.index, df['Close'] - df['Anomaly_Error'], df['Close'] + df['Anomaly_Error'], color='red', alpha=0.2, label='Anomaly Error')
+    plt.legend()
+    plt.title('TON Coin with Errors')
+    plt.grid(True)
     plt.show()
 
 if __name__ == "__main__":
@@ -54,4 +51,5 @@ if __name__ == "__main__":
     save_to_csv(data)
     plot_trend(data)
     print(get_statistics(data))
-    model_data(data)
+    data_with_errors = calculate_errors(data)
+    plot_errors(data_with_errors)
